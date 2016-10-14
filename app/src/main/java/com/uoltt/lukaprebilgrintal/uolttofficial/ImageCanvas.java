4e84b7b3bd8caa06d0b1c3d4c4bd8226ef02f540
LukaPrebil <lp7421@student.uni-lj.si>
2016-10-14 16:08:57 +0200

    added an options menu removed random xml file

--- app/src/main/java/com/uoltt/lukaprebilgrintal/uolttofficial/ImageCanvas.java
+++ app/src/main/java/com/uoltt/lukaprebilgrintal/uolttofficial/ImageCanvas.java
@@ -5,6 +5,9 @@ import android.media.Image;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -56,5 +59,29 @@ public class ImageCanvas extends AppCompatActivity {
             }
         };
         formimage.post(viewsUpdateTask);
+    }
+
+
+    // Menu Stuff
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.logout:
+                //logout();
+                return true;
+            case R.id.settings:
+                //showSettings();
+                return true;
+            case R.id.credits:
+                //openCreditsActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
