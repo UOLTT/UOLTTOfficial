880e0c5acc05e19769711c225298cda3eed6df85
Luka Prebil <lp7421@student.uni-lj.si>
2017-04-30 17:04:49 +0200

    can finally login by pressing enter on the soft keyboard, instead of the login button

--- app/src/main/java/com/uoltt/lukaprebilgrintal/uolttofficial/LoginActivity.java
+++ app/src/main/java/com/uoltt/lukaprebilgrintal/uolttofficial/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 
@@ -72,6 +73,56 @@ public class LoginActivity extends AppCompatActivity {
             UserData.squadronID = prefs.getInt("SquadronID", -1);
             UserData.token = prefs.getString("Token", "TKNError");
             switchActivity();
+        }
+        else {
+            EditText tokenField = (EditText) findViewById(R.id.tokenField);
+            tokenField.setOnKeyListener(new View.OnKeyListener() {
+                @Override
+                public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                    if (keyEvent.getAction() == KeyEvent.ACTION_DOWN)
+                    {
+                        switch (keyCode)
+                        {
+                            case KeyEvent.KEYCODE_DPAD_CENTER:
+                            case KeyEvent.KEYCODE_ENTER:
+                                Object lock = new Object();
+                                EditText tokenField = (EditText) findViewById(R.id.tokenField);
+                                String token = tokenField.getText().toString();
+                                new CheckTokenValidity().execute(token);
+                                try{
+                                    synchronized (lock){
+                                        lock.wait(500);
+                                    }
+
+                                    if(UserData.tokenValidity){
+
+                                        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_preferences_file), Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+                                        editor.putString("Username", UserData.username);
+                                        editor.putString("Squadron", UserData.squadName);
+                                        editor.putInt("SquadronID",  UserData.squadronID);
+                                        editor.putString("Token",    UserData.token);
+                                        editor.putBoolean("LoggedIn", true);
+                                        editor.apply();
+                                        switchActivity();
+                                    } else {
+                                        String invalidToken = "No user with this token exists.";
+                                        Snackbar snackbar = Snackbar.make(findViewById(R.id.login_layout),
+                                                invalidToken, Snackbar.LENGTH_LONG);
+                                        snackbar.show();
+                                    }
+                                } catch (Exception e){
+                                    System.err.println("error in loginactivity");
+                                    System.err.println(e.getMessage());
+                                }
+                                return true;
+                            default:
+                                break;
+                        }
+                    }
+                    return false;
+                }
+            });
         }
     }
 }
